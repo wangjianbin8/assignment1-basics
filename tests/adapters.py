811eb85358fe7ef6b3e18a751c0db9b96a50e2bb
wangjianbin8 <1493701979@qq.com>
2026-09-15 15:00:48 +0800

    测试

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,7 +8,9 @@ import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
-from cs336_basics.bpe import train_bpe
+
+from cs336_basics.hw1.bpe import train_bpe
+from cs336_basics.hw1.tokenizer_v3 import Tokenizer
 
 
 def run_linear(
@@ -537,7 +539,7 @@ def run_load_checkpoint(
     Returns:
         int: the previously-serialized number of iterations.
     """
-    raise NotImplementedError
+    raise 
 
 
 def get_tokenizer(
@@ -560,7 +562,7 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    return Tokenizer(vocab, merges, special_tokens)
 
 
 def run_train_bpe(
